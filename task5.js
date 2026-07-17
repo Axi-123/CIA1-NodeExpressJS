78a8288e20d1915f9a5ff9126b34de47f30b41be
Axi-123 <axilia.jennifer@btech.christuniversity.in>
2026-07-17 15:09:48 +0530

    Add Task 5

---
+++ task5.js
@@ -0,0 +1,10 @@
+// Task 5: Student Record System - Classify a Record
+
+let studentName = "Axilia Jennifer B";
+let marks = 78;
+
+if (marks >= 40) {
+    console.log(studentName + " has Passed.");
+} else {
+    console.log(studentName + " has Failed.");
+}
