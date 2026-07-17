c358bf9b337569a51656ebc08a567fc9de6bc762
Axi-123 <axilia.jennifer@btech.christuniversity.in>
2026-07-17 15:08:28 +0530

    Add Task 3

---
+++ task3.js
@@ -0,0 +1,14 @@
+// Task 3: Student Record System - Identify Datatypes
+
+let studentName = "Axilia Jennifer B";
+let studentMarks = 92;
+let isPassed = true;
+
+console.log("Student Name:", studentName);
+console.log("Datatype:", typeof studentName);
+
+console.log("Student Marks:", studentMarks);
+console.log("Datatype:", typeof studentMarks);
+
+console.log("Pass Status:", isPassed);
+console.log("Datatype:", typeof isPassed);
