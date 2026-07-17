d3282643626b4d479bc96040364a5fc0930bd7b2
Axi-123 <axilia.jennifer@btech.christuniversity.in>
2026-07-17 15:17:22 +0530

    Add Task 10

---
+++ task10.js
@@ -0,0 +1,15 @@
+// Task 10: Student Record System - Create and Print an Object
+
+let student = {
+    id: 1,
+    name: "Axilia Jennifer B",
+    age: 20,
+    marks: 92,
+    department: "Computer Science"
+};
+
+console.log("Student ID:", student.id);
+console.log("Student Name:", student.name);
+console.log("Age:", student.age);
+console.log("Marks:", student.marks);
+console.log("Department:", student.department);
