29f3687adda2026c6d112fbe02165f839051ee6f
Axi-123 <axilia.jennifer@btech.christuniversity.in>
2026-07-17 15:07:42 +0530

    add Task 2

---
+++ task2.js
@@ -0,0 +1,7 @@
+// Task 2: Student Record System - Declare and Update Variables
+
+let systemName = "Student Record System";
+let totalStudents = 4;
+
+console.log("System Name:", systemName);
+console.log("Total Number of Students:", totalStudents);
